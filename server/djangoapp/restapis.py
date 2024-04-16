a0c40db5fe1339c21c07b5d90c4790e90d4a4b5f
maina-duseja <37204269+maina-duseja@users.noreply.github.com>
2024-04-16 12:57:36 +0530

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,5 +1,5 @@
 # Uncomment the imports below before you add the function code
-# import requests
+import requests
 import os
 from dotenv import load_dotenv
 
@@ -11,12 +11,13 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
+
 # Add code for get requests to back end
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+    if (kwargs):
+        for key, value in kwargs.items():
+            params = params+ key+ "="+ value+ "&"
 
     request_url = backend_url+endpoint+"?"+params
 
@@ -25,9 +26,11 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except Exception as e:    
         # If any error occurs
+        print(f"Unexpected {e}, {type(e)=}")
         print("Network exception occurred")
+
 
 # Add code for retrieving sentiments
 def analyze_review_sentiments(text):
@@ -40,6 +43,7 @@ def analyze_review_sentiments(text):
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
+
 # Add code for posting review
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
@@ -47,5 +51,6 @@ def post_review(data_dict):
         response = requests.post(request_url,json=data_dict)
         print(response.json())
         return response.json()
-    except:
+   except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
